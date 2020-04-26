bcbb2e60b65a84b9ac2732efc15db91ce5a5a8fa
Lenovo <317144967@qq.com>
2020-04-26 14:17:21 +0800

    test git 提交 01

--- idea-git-test/src/main/java/com/atguigu/test/Hello.java
+++ idea-git-test/src/main/java/com/atguigu/test/Hello.java
@@ -7,10 +7,11 @@ package com.atguigu.test;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("你好git");
-        System.out.println("你好git");
-        System.out.println("你好git");
-        System.out.println("你好git");
-        System.out.println("你好git");
+        System.out.println("你好git01");
+        System.out.println("你好git01");
+        System.out.println("你好git01");
+        System.out.println("你好git01");
+        System.out.println("你好git01");
+       
     }
 }
