351e6cf9bd3a100086e4ebc3c8be6aef54b2a691
Lenovo <317144967@qq.com>
2020-04-26 12:08:07 +0800

    更改了代码 hello git002

---
+++ idea-git-test/src/main/java/com/atguigu/test/Hello2.java
@@ -0,0 +1,12 @@
+package com.atguigu.test;
+
+/**
+ * \* To change this template use File | Settings | File Templates.
+ * \*
+ * \
+ */
+public class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("ni hao de2 ");
+    }
+}
