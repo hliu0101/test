f46d042f19a4abf6332df1740952e03f41fb9d83
Lenovo <317144967@qq.com>
2020-04-26 11:35:28 +0800

    test git 提交 01

---
+++ idea-git-test/src/main/java/com/atguigu/test/Hello.java
@@ -0,0 +1,16 @@
+package com.atguigu.test;
+
+/**
+ * \* To change this template use File | Settings | File Templates.
+ * \*
+ * \
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("你好git");
+        System.out.println("你好git");
+        System.out.println("你好git");
+        System.out.println("你好git");
+        System.out.println("你好git");
+    }
+}
